c45a37894fe3e85d432d052706dcfda03ddd736f
Summy <s4594789@student.uq.edu.au>
2023-11-11 13:58:10 +1000

    implement api and display data in table

---
+++ src/components/CurrencyTable/CurrencyTable.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styles from "./CurrencyTable.module.scss";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { useSelector } from "react-redux";
+
+export default function CurrencyTable() {
+  const crypto = useSelector((state) => state.crypto);
+
+  return (
+    <div className={styles.tableOuterContainer}>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <button className={styles.headingButton}>#</button>
+              </TableCell>
+              <TableCell align="left">
+                <button className={styles.headingButton}>Coin</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>Price</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>24h</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>7d</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>1mth</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>24h Vol</button>
+              </TableCell>
+              <TableCell align="center">
+                <button className={styles.headingButton}>Mkt Cap</button>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {crypto.currenciesStats.map((currencyStats) => (
+              <TableRow
+                key={currencyStats.symbol}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  1
+                </TableCell>
+                <TableCell align="center" className={styles.tableCell}>
+                  <img
+                    className={styles.cryptoImg}
+                    src={currencyStats.image}
+                    alt={currencyStats.symbol}
+                  />
+                  {currencyStats.symbol}
+                </TableCell>
+                <TableCell align="center">
+                  {currencyStats.currentPrice}
+                </TableCell>
+                <TableCell align="center">
+                  {currencyStats.price24hChangePercentage}%
+                </TableCell>
+                <TableCell align="center">
+                  {currencyStats.price7dChangePercentage}%
+                </TableCell>
+                <TableCell align="center">
+                  {currencyStats.price1mthChangePercentage}%
+                </TableCell>
+                <TableCell align="center">{currencyStats.volume}</TableCell>
+                <TableCell align="center">{currencyStats.marketCap}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "./Homepage.module.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import Heading from "../../components/Heading/Heading";
+import CurrencyTable from "../../components/CurrencyTable/CurrencyTable";
+import { getAllCurrencyStats } from "../../api/crypto/crypto";
+import { CircularProgress } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { cryptoActions } from "../../store/modules/cryptoSlice";
 
 export default function Homepage() {
+  const crypto = useSelector((state) => state.crypto);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (crypto.currenciesStats.length > 0) {
+      return;
+    }
+    const fetchCurrencies = async () => {
+      const result = await getAllCurrencyStats();
+      dispatch(cryptoActions.setCryptoCurrenciesStats(result.data));
+    };
+    fetchCurrencies();
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log(crypto.currenciesStats);
+    console.log(crypto.currenciesStats.length);
+  }, [crypto.currenciesStats]);
+
   return (
     <>
       <Navbar />
-      <Heading />
+      {crypto.currenciesStats.length > 0 ? (
+        <>
+          <Heading />
+          <CurrencyTable />
+        </>
+      ) : (
+        <div className={styles.loadingBarContainer}>
+          <CircularProgress size={100} sx={{ marginTop: 30 }} />
+        </div>
+      )}
     </>
   );
 }
